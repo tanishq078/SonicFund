bec28913183f0dd9b69c02e3f359325633b9b0dd
tanishq078 <stanishq69@gmail.com>
2024-12-14 15:06:43 +0530

    removing link

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://stanishq696969:u4vkB7xEkTjUYzdU@cluster0.5m4lgjn.mongodb.net/")
+mongoose.connect("use your own db link")
 
 const userSchema=new mongoose.Schema({
     username:String,
