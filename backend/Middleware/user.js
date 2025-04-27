6dbb6dfd4062bd62e5717333fba389c0c25450b6
tanishq078 <stanishq69@gmail.com>
2025-04-28 04:47:56 +0530

    server call

--- backend/Middleware/user.js
+++ backend/Middleware/user.js
@@ -1,21 +1,24 @@
-const { User } = require("../db");
+const {User} = require("../db")
+async function usermiddleware(req,res,next){
+    const username=req.headers.username
+    const password = req.headers.password
 
-async function usermiddleware(req, res, next) {
-    const { username, password } = req.headers;
-
-    try {
-        const user = await User.findOne({ username, password });
-
-        if (user) {
-            req.user = user;
+   const user =  await User.findOne({
+        username:username,
+        password: password
+    })
+    
+    .then(function(user){
+        if(user){
+            
+            
+          req.user=user
             next();
-        } else {
-            res.status(404).json({ msg: "User does not exist" });
         }
-    } catch (err) {
-        console.error("Error in usermiddleware:", err);
-        res.status(500).json({ msg: "Internal server error" });
-    }
+        else{
+            res.json({msg:"user does not exist"})
+            
+        }
+    })
 }
-
-module.exports = usermiddleware;
+module.exports= usermiddleware
